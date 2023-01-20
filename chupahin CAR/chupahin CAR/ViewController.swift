b2ed70281284fa0f1628aeb988d3264974488ba0
Deus210 <103108069+Deus210@users.noreply.github.com>
2023-01-20 16:06:01 +0300

    Add files via upload

---
+++ chupahin CAR/chupahin CAR/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  chupahin CAR
+//
+//  Created by Чупахин Аркадий Александрович on 20/1/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
